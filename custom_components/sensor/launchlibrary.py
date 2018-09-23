33b8436899b6122db15c54e7552df71648c36730
ludeeus <joasoe@gmail.com>
2018-09-23 17:44:10 +0200

    Added exceptions for IndexError

--- custom_components/sensor/launchlibrary.py
+++ custom_components/sensor/launchlibrary.py
@@ -1,68 +1,81 @@
 """
 A component which allows you to get information about next launches
+
 For more details about this component, please refer to the documentation at
 https://github.com/HalfDecent/HA-Custom_components/launchlibrary
 """
-import requests
-import voluptuous as vol
 from datetime import timedelta
+import requests
 from homeassistant.helpers.entity import Entity
 
-_version__ = '1.0.2'
+__version__ = '1.1.0'
 
 ATTR_STREAM = 'stream'
 ATTR_LAUNCH_NAME = 'launch_name'
 ATTR_LAUNCH_TIMESTAMP = 'timestamp'
-ATTR_AGENCY_NAME = 'agengy'
-ATTR_AGENCY_COUNTRY = 'agengy_country_code'
+ATTR_AGENCY_NAME = 'agency'
+ATTR_AGENCY_COUNTRY = 'agency_country_code'
 
 SCAN_INTERVAL = timedelta(seconds=60)
 
-ICON = 'mdi:rocket'
-
 def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the platform"""
     add_devices([LaunchSensor()])
 
 class LaunchSensor(Entity):
+    """The sensor class."""
     def __init__(self):
-        self._state = None
-        self._launchtimestamp = None
-        self._launchname = None
-        self._agencyname = None
-        self._agencycountry = None
-        self._launchstream = None
         self.update()
 
     def update(self):
+        """Update the sensor values."""
         baseurl = "https://launchlibrary.net/1.4/"
         fetchurl = baseurl + 'launch/next/1'
         launch = requests.get(fetchurl).json()['launches'][0]
-        self._state = launch["windowstart"]
-        self._launchtimestamp = launch["wsstamp"]
-        self._launchname = launch["name"]
-        self._agencyname = launch["location"]["pads"][0]["agencies"][0]["name"]
-        self._agencycountry = launch["location"]["countryCode"]
         try:
-            launch["vidURLs"][0]
-        except Exception:
-            self._launchstream = None
-        else:
+            self._state = launch["windowstart"]
+        except IndexError:
+            self._state = None
+        try:
+            self._launchtimestamp = launch["wsstamp"]
+        except IndexError:
+            self._launchtimestamp = None
+        try:
+            self._launchname = launch["name"]
+        except IndexError:
+            self._launchname = None
+        try:
+            self._agencyname = (launch["location"]["pads"]
+                                [0]["agencies"][0]["name"])
+        except IndexError:
+            self._agencyname = None
+        try:
+            self._agencycountry = launch["location"]["countryCode"]
+        except IndexError:
+            self._agencycountry = None
+        try:
             self._launchstream = launch["vidURLs"][0]
+        except IndexError:
+            self._launchstream = None
 
     @property
     def name(self):
+        """Return the name of the sensor."""
         return 'nextlaunch'
 
     @property
     def state(self):
+        """Return the state of the sensor."""
         return self._state
 
     @property
     def icon(self):
-        return ICON
+        """Return the icon of the sensor."""
+        return 'mdi:rocket'
 
     @property
     def device_state_attributes(self):
+        """Return attributes of the sensor."""
         return {
             ATTR_LAUNCH_NAME: self._launchname,
             ATTR_LAUNCH_TIMESTAMP: self._launchtimestamp,
